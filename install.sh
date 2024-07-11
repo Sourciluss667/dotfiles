ebbb818540bc428da4bd6034c87200bc369be60d
Quentin Cormand <quentin@gotomorro.com>
2024-07-11 13:07:43 +0200

    add git config, improve install script

--- install.sh
+++ install.sh
@@ -12,8 +12,16 @@ echo "Volta installation done!"
 
 
 echo "Install some packages..."
-brew install lsd git neovim fish
+brew install lsd git neovim fish eslint_d prettierd gitmux luarocks fd ripgrep htop lazygit docker docker-compose
 echo "Packages installation done!"
+
+echo "Install Nerd font..."
+brew install --cask font-hack-nerd-font
+echo "Nerd font installation done!"
+
+echo "Install Oh My Fish..."
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+echo "OMF installation done!"
 
 echo "Configure..."
 
